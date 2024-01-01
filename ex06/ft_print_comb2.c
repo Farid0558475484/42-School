a7984104e4be791b756ac69d869f10521d2648c4
Hajizada Farid <faridmh@code.edu.az>
2024-01-01 19:35:06 +0400

    feat:update ft_print_comb

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -3,7 +3,8 @@
 void write_n(short n) {
     char c;
     if (n < 10) {
-        c = n + '0';
+        c = n + 48;
+        write(1, "0", 1);
         write(1, &c, 1);
         return;
     }
@@ -21,17 +22,15 @@ void ft_print_comb2(void) {
         v[1] = v[0] + 1;
         while (v[1] < 100) {
 
-            if (v[1] > v[0]) {
                 write_n(v[0]);
                 write(1, " ", 1);
                 write_n(v[1]);
 
                 if (v[0] == 98 && v[1] == 99) {
-                    write(1, ".\n", 2);
+                    write(1, " \n", 2);
                 } else {
                     write(1, ", ", 2);
                 }
-            }
             v[1]++;
         }
         v[0]++;
